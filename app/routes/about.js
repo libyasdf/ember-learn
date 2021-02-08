551b7ccb500f76a1026b49c841d9dacfbb3ee60a
Baiyue Li <liby1994@126.com>
2021-02-08 21:52:53 +0800

    component part1

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 
-export default class ScientistsRoute extends Route {
+export default class AboutRoute extends Route {
   model() {
     return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
   }
